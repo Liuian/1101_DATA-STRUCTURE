f1f66cff2d23888cfd2ea9086b2f33095f087882
Liuian <liuperong@gmail.com>
2021-12-21 14:09:51 +0800

    finish HW3-1

--- HW3/hw3-1/hw3-1.cpp
+++ HW3/hw3-1/hw3-1.cpp
@@ -33,8 +33,8 @@ int search(int arr[], int strt, int end, int value){
 }
 
 /* Recursive function to construct binary of size len from Inorder traversal in[] and Preorder traversal pre[]. Initial values of inStrt and inEnd should be 0 and len -1. The function doesn't do any error checking for cases where inorder and preorder do not form a tree */
+int preIndex;
 node* buildTreePre(int in[], int pre[], int inStrt, int inEnd){
-	static int preIndex = 0; 
 	if (inStrt > inEnd)
 		return NULL;
 	/* Pick current node from Preorder traversal using preIndex and increment preIndex */
@@ -51,14 +51,14 @@ node* buildTreePre(int in[], int pre[], int inStrt, int inEnd){
 }
 /*----------------------------to change--------------------*/
 /* Recursive function to construct binary of size len from Inorder traversal in[] and Preorder traversal pre[]. Initial values of inStrt and inEnd should be 0 and len -1. The function doesn't do any error checking for cases where inorder and preorder do not form a tree */
+int postIndex;
 node* buildTreePost(int in[], int post[], int inStrt, int inEnd){
-    static int postIndex = 0;
     
 	if (inStrt > inEnd)
         return NULL;
     
 	/* Pick current node from Preorder traversal using preIndex and increment preIndex */
-    node* tNode = newNode(post[postIndex++]);
+    node* tNode = newNode(post[postIndex--]);
     
 	/* If this node has no children then return */
     if (inStrt == inEnd)
@@ -68,8 +68,8 @@ node* buildTreePost(int in[], int post[], int inStrt, int inEnd){
     int inIndex = search(in, inStrt, inEnd, tNode->data);
     
 	/* Using index in Inorder traversal, construct left and right subtress */
-    tNode->left = buildTreePost(in, post, inStrt, inIndex - 1);
     tNode->right = buildTreePost(in, post, inIndex + 1, inEnd);
+    tNode->left = buildTreePost(in, post, inStrt, inIndex - 1);
     
 	return tNode;
 }
@@ -90,15 +90,16 @@ void printPostorder(node* node){
 void printPreorder(node* node){
     if (node == NULL)
         return;
+    /* then print the data of node */
+    cout<< node->data <<" ";
     /* first recur on left child */
     printPreorder(node->left);
     /* now recur on right child */
     printPreorder(node->right);
-    /* then print the data of node */
-    cout<< node->data <<" ";
 }
 
 int main(){
+	cout << "please type input file's name : ";
 	string input;
 	cin >> input;
 	ifstream InputFile(input);
@@ -121,12 +122,16 @@ int main(){
 		}
 		node* root = NULL;
 		if(str == "preorder-inorder"){
+			preIndex = 0;
 			root = buildTreePre(second, first, 0, NodeCount - 1);
 			printPostorder(root);
+			cout << '\n';
 		}
 		if(str == "postorder-inorder"){
-			//root = 
+			postIndex = NodeCount - 1;
+			root = buildTreePost(second, first, 0, NodeCount - 1);
 			printPreorder(root);
+			cout << '\n';
 		}
 	}
 	return 0;
